0d2fc4fcc5f55404be760cf73c4a2df9f26869b9
LLeo <lleoem@gmail.com>
2015-11-26 19:37:58 -0500

    changed "lookup" to "readdir"

--- routes/readdir.js
+++ routes/readdir.js
@@ -20,7 +20,7 @@ Promise.promisifyAll(fs)
  */
 function filterFilesByExt(files, exts) {
   'use strict';
-  console.log("lookup: filterFilesByExt: exts = %j", exts)
+  console.log("readdir: filterFilesByExt: exts = %j", exts)
   return files.filter(function(fn) {
     for (let ext of exts)
       if (path.extname(fn).substr(1) == ext)
@@ -35,28 +35,30 @@ module.exports = function (req, res) {
   
   var cfg = req.app.get('app config by name')
 
-  console.log("lookup: cfg = ", cfg)
+  console.log("readdir: cfg = ", cfg)
 
   var root = req.query.root
   var root_fqdn = cfg['video roots'][root].fqdn
   var subdirs = u.clone(req.query.subdirs) || []
   var exts = cfg['acceptable extensions']
-  console.log("lookup: exts = %j", exts)
+  console.log("readdir: exts = %j", exts)
 
   // Stop any hacking attempt to '..' below '/'
-  console.log('lookup: subdirs = %j', subdirs)
+  console.log('readdir: subdirs = %j', subdirs)
 
   subdirs.unshift('/')
   var nmldir = path.join.apply(path, subdirs)
+  nmldir = nmldir.slice(1) // take off the /
+  subdirs.shift()
 
-  console.log('lookup: nmldir = %s', nmldir)
+  console.log('readdir: nmldir = %s', nmldir)
 
   var fqdn = path.join(root_fqdn, nmldir)
 
-  console.log("lookup: fqdn = %j", fqdn)
+  console.log("readdir: fqdn = %j", fqdn)
   
   fs.readdirAsync(fqdn).map(function(fileName) {
-    console.log("lookup: fileName = %j", fileName)
+    console.log("readdir: fileName = %j", fileName)
     var fqfn = path.join(fqdn, fileName)
     var stat = fs.statAsync(fqfn);
 
@@ -89,7 +91,7 @@ module.exports = function (req, res) {
                }
       , json_str = JSON.stringify(json)
     
-    console.log("lookup: sending json:\n"
+    console.log("readdir: sending json:\n"
                , util.inspect(json, {depth:null}))
     
     res.set("Content-type", "application/json")
