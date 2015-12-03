b7a7987db98a547e39d1bdcf394cc01da8a0dfdc
LLeo <lleoem@gmail.com>
2015-12-02 21:47:49 -0500

    made it all work and commented-out/delete most log statments

--- routes/findtrack.js
+++ routes/findtrack.js
@@ -23,7 +23,7 @@ const MODNAME = path.parse(module.filename).name
  */
 function filterFilesByExt(files, exts) {
   'use strict';
-  console.log("%s: filterFilesByExt: exts = %j", MODNAME, exts)
+  //console.log("%s: filterFilesByExt: exts = %j", MODNAME, exts)
   return files.filter(function(fn) {
     for (let ext of exts) {
       // test the file's extension name (minus the leading '.')
@@ -35,30 +35,75 @@ function filterFilesByExt(files, exts) {
   })
 }
 
-function trackMatchesVideo(trackFile, videoFile) {
-  return false
+function trackParse(trackfn) {
+  var trackParts = trackfn.split('.')
+  if (trackParts.length < 3) {
+    //badly formated trackfn
+    return
+  }
+  var ext = trackParts[trackParts.length-1]
+  var langPart = trackParts[trackParts.length-2]
+  var m = langPart.match(/^[a-z]{2}/)
+  if (!m) {
+    //bad langPart
+    return
+  }
+  var lang = m[0]
+
+  var desc = lang
+  if (lang == 'en') {
+    desc = "English"
+  }
+
+  if ( langPart.match(/foreign parts/) ) {
+    desc += " Foreign Parts Only"
+  }
+
+  return { name: trackfn
+         , base: trackParts.slice(0,trackParts.length-2).join('.')
+         , lang: lang
+         , desc: desc
+         }
 }
 
-function describeTrack(trackFile) {
-  return 'nothing'
+function videoParse(videofn) {
+  //console.log('%s: videoParse: videofn=%j', MODNAME, videofn)
+  if ( videofn.match(/\//) ) {
+    console.log('%s: videoParse: videofn %j contained a "/"', MODNAME, videofn)
+    return
+  }
+
+  var videoParts = videofn.split('.')
+  if (videoParts.length < 2 || videoParts[0] == '') {
+    //badly formed videofn
+    console.log('%s: videoParse: badly formed videofn=%j', MODNAME, videofn)
+    return
+  }
+  return { name: videofn
+         , ext : videoParts[videoParts.length-1]
+         , base: videoParts.slice(0,videoParts.length-1).join('.')
+         }
 }
 
-function langFromTrack(trackFile) {
-  return 'en'
+function sortFn(a,b) {
+  var aExt = path.extname(a.uri).slice(1)
+  var bExt = path.extname(b.uri).slice(1)
+  if (aExt == 'vtt' && bExt == 'srt') return -1
+  if (aExt == 'srt' && bExt == 'vtt') return 1
+  return 0
 }
 
 module.exports = function findtrack(req, res, next) {
   'use strict';
-  console.log('%s: req.originalUrl = %j', MODNAME, req.originalUrl)
+  //console.log('%s: req.originalUrl = %j', MODNAME, req.originalUrl)
 
   var cfg = req.app.get('app config by name')
 
-  console.log("%s: cfg = ", MODNAME, cfg)
+  //console.log("%s: cfg = ", MODNAME, cfg)
 
   var root = req.query.root
   var subdirs = req.query.subdirs
-  var videoFile = req.query.file
-  
+  var videofn = req.query.file
   // See routes/index.js for how I came up with this rx
   var rx = /(?:\/\.\.(?![^\/])|^\.\.(?![^\/])|^\.\.$)/;
 
@@ -78,31 +123,33 @@ module.exports = function findtrack(req, res, next) {
     }
   }
 
-  if (videoFile.match(rx)) {
-    console.error('%s: HACKING ATTEMPT videoFile=%j matched %s', MODNAME
-                 , videoFile, rx.toString())
+  if (videofn.match(rx)) {
+    console.error('%s: HACKING ATTEMPT videofn=%j matched %s', MODNAME
+                 , videofn, rx.toString())
     res.status(403).end('FUCK OFF')
     return
   }
 
+  var vid = videoParse(videofn)
+  if (!vid) {
+    console.error('%s: findtrack: badly formed videofn=%j', MODNAME, videofn)
+    res.status(404).end('badly formed video filename')
+    return
+  }
+  
   var root_fqdn = cfg['video roots'][root].fqdn
 
-  //subdirs.unshift('/')
-  //var nmldir = path.join.apply(path, subdirs)
-  //nmldir = nmldir.slice(1) // take off the '/' from nmldir
-  //subdirs.shift() // remove the '/' entry from subdirs
-  //console.log('%s: nmldir = %s', MODNAME, nmldir)
-  //var fqdn = path.join(root_fqdn, nmldir)
+  //console.log('%s: root_fqdn = %j', MODNAME, root_fqdn)
 
   var parts = [root_fqdn]
   parts = parts.concat(subdirs)
   
   var fqdn = path.join.apply(path, parts)
 
-  console.log('%s: fqdn = %j', MODNAME, fqdn)
+  //console.log('%s: fqdn = %j', MODNAME, fqdn)
 
   fs.readdirAsync(fqdn).map(function(fileName) {
-    console.log('%s: fileName = %j', MODNAME, fileName)
+    //console.log('%s: fileName = %j', MODNAME, fileName)
     var fqfn = path.join(fqdn, fileName)
     var stat = fs.statAsync(fqfn)
 
@@ -118,35 +165,60 @@ module.exports = function findtrack(req, res, next) {
       }
     })
 
-    console.log('%s: before filter: files = %j', MODNAME, files)
-    files = filterFilesByExt(files, ['vtt'])
-    console.log('%s: after filter: files = %j', MODNAME, files)
+    //console.log('%s: before filter: files = %j', MODNAME, files)
+    files = filterFilesByExt(files, ['vtt', 'srt'])
+    //console.log('%s: after filter: files = %j', MODNAME, files)
 
-    // files array now only contains '*.vtt' file names
+    // files array now only contains '*.vtt' and/or '*.srt' file names
     
     var tracks = []
-    files.forEach(function(trackFile) {
-      var parts, uri, label, lang
-      if ( trackMatchesVideo(trackFile, videoFile) ) {
+    files.forEach(function(trackfn) {
+      //console.log('%s: trackfn = %j', MODNAME, trackfn)
+      //console.log('%s: videofn = %j', MODNAME, videofn)
+      var trk = trackParse(trackfn)
+      if (!trk) {
+        console.log('%s: badly formed trackfn=%j', MODNAME, trackfn)
+        return //skip this trackfn
+      }
+      
+      if ( vid.base == trk.base ) {
+        //console.log('%s: the files matched', MODNAME)
+        //console.log('%s: trk = %j', MODNAME, trk)
+
         parts = ['/track', root]
         parts = parts.concat(subdirs)
-        parts.push(trackFile)
+        parts.push(trackfn)
 
-        uri = path.join.apply(path, parts)
+        var uri = path.join.apply(path, parts)
 
-        label = describeTrack(trackFile)
-
-        lang = langFromTrack(trackFile)
-
-        tracks.push({ uri: uri, label: label, lang: lang })
+        tracks.push({ uri: uri, label: trk.desc, lang: trk.lang })
       }
+      //else
+      //  console.log('%s: the files DID NOT match', MODNAME)
     })
 
-    // NOTE: for testing we send no tracks
-    var json = { tracks : [] }
+    //console.log('%s: before sort tracks =\n%s', MODNAME, util.inspect(tracks, {depth:null,colors:true}))
+    var foreign_tracks = []
+    var nonforeign_tracks = []
+
+    if (tracks.length > 1) {
+      tracks.forEach(function(track) {
+        if (track.label.match(/Only/))
+          foreign_tracks.push(track)
+        else
+          nonforeign_tracks.push(track)
+      })
+      foreign_tracks.sort(sortFn)
+      nonforeign_tracks.sort(sortFn)
+      tracks = foreign_tracks.concat(nonforeign_tracks)
+    }
+
+    //console.log('%s: after sort tracks =\n%s', MODNAME, util.inspect(tracks, {depth:null,colors:true}))
+    
+    var json = { tracks : tracks }
     var json_str = JSON.stringify(json)
 
-    console.log('%s: sending json\n', MODNAME
+    console.log('%s: sending json\n%s', MODNAME
                , util.inspect(json, {depth:null}))
 
     res.set('Content-type', 'application/json')
